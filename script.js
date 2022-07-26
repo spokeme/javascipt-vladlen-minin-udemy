8f3d9a30415cca44c5a6643c9a80d8195e057368
spokeme <32917066+spokeme@users.noreply.github.com>
2022-07-27 01:14:17 +0300

    6 новых уроков
    введение и основа

---
+++ script.js
@@ -0,0 +1,18 @@
+var message = 'Hello World! 2'
+var helloMessage = 'Hello again'
+// CamelCase
+
+var $ = 'Dollar'
+var _ = 'Underscore'
+
+var message2 = 'Hello world message 2'
+
+// Можно писать без точки с запятой в конце строчки
+
+
+var message3 = "Hello world message 3"
+//Одинаковые ковычки
+
+
+
+console.log(message)
